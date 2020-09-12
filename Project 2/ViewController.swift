98b8380bcaa857c36a5b9a5e775d6d1be24aff6c
hvaftrudner <krill_eriksson@hotmail.com>
2020-09-12 14:17:44 +0200

    updated with share functionality

--- Project 2/ViewController.swift
+++ Project 2/ViewController.swift
@@ -10,11 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score: Int = 0
+    var correctAnswer: Int = 0
+    var total: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany","ireland", "italy", "monaco", "poland", "nigeria", "russia", "spain", "uk", "us"]
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(checkScore))
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil){
+        
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = "Guess -- \(countries[correctAnswer].uppercased()) -- Your score is \(score)"
+        
     }
 
-
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+            total += 1
+        } else {
+            title = "Wrong! That was \(countries[sender.tag])"
+            score -= 1
+            total += 1
+        }
+        
+        if total == 10 {
+            let newAc = UIAlertController(title: "Finished", message: "10 rounds have been played your score was \(score) out of 10", preferredStyle: .alert)
+            newAc.addAction(UIAlertAction(title: "Restart?", style: .default, handler: askQuestion))
+            present(newAc, animated: true)
+            
+            score = 0
+            
+        } else {
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        }
+        
+        
+        print(total)
+        
+        
+    }
+    @objc func checkScore() {
+        
+        let vc = UIActivityViewController(activityItems: [String("your score is \(score)")], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 }
 
